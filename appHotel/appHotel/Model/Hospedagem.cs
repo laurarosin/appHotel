5009d6041527dc3d11dd141ea480a45ee93fd163
Usuario <Usuario@CPD03-M03>
2022-08-31 15:32:09 -0300

    Adicionar arquivos de projeto.

---
+++ appHotel/appHotel/Model/Hospedagem.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace appHotel.Model
+{
+    public class Hospedagem
+    {
+        Suite quarto;
+        int Quantidade_adultos;
+
+
+        public int QntAdultos
+        {
+            get => Quantidade_adultos;
+
+            set
+            {
+                if (value == 0)
+                    throw new Exception("Por favor, informe a quantidade de adultos:");
+                Quantidade_adultos = value;
+            }
+        }
+        public int QuantidadeCrianca { get; set; }
+
+        public Suite Quarto
+        {
+            get => quarto;
+
+            set
+            {
+                if (value == null)
+                    throw new Exception("Por favor, selecione uma suíte:");
+                quarto = value;
+            }
+        }
+
+        public DateTime DataCheckIn { get; set; }
+        public DateTime DataCheckOut { get; set; }
+
+        public int Estadia
+        {
+            get
+            {
+                return DataCheckOut.Subtract(DataCheckIn).Days;
+            }
+        }
+        public double ValorTotal
+        {
+            get => ((Quantidade_adultos * Quarto.ValorDiariaAdulto) + ((QuantidadeCrianca * Quarto.ValorDiariaCrianca)) + Estadia;
+
+        }
+
+    }
+}
---
+++ appHotel/appHotel/Model/Suite.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace appHotel.Model
+{
+    internal class Suite
+    {
+    }
+}
